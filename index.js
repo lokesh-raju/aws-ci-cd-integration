26d0545adcf9baa910b790bc7fb874edb1ccfb93
Lokesh <lokessri@cisco.com>
2021-02-23 14:19:11 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express()
-const port = 3100;
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => res.send('Welcome!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
