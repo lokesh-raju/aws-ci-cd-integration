3bb5f54148aeec69f2e88755b44f69eb5df85d3a
Lokesh <lokessri@cisco.com>
2021-02-23 15:53:06 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'aws-codedeploy',
+      name: 'aws-codedeploy-lokessri',
       script: 'npx',
       args: 'serve -s build -l 3000 -n',
       interpreter: 'none',
